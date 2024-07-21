fd6e58866868beb6e5bbaf0ab0f36447754049b7
Shreyash Soni <yashsoni96@gmail.com>
2024-07-21 13:23:53 +0530

    add helper code for data handling

---
+++ 002_package_model/prediction_model/processing/data_handling.py
@@ -0,0 +1,26 @@
+import os
+import joblib
+import pandas as pd
+
+from prediction_model.config import config
+
+
+# Load the data
+def load_dataset(file_name):
+    filepath = os.path.join(config.DATA_PATH, file_name)
+    return pd.read_csv(filepath)
+
+
+# Serialization
+def save_pipeline(pipeline_to_save):
+    save_path = os.path.join(config.MODEL_SAVE_PATH, config.MODEL_NAME)
+    joblib.dump(pipeline_to_save, save_path)
+    print(f"Model saved by the name {config.MODEL_NAME}...")
+
+
+# Deserialization
+def load_pipeline(pipeline_to_load):
+    pipeline_path = os.path.join(config.MODEL_SAVE_PATH, config.MODEL_NAME)
+    model = joblib.load(pipeline_path)
+    print("Model successfully loaded...")
+    return model

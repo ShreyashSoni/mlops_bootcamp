42aa5c88549dfa3d65dec558ad0d37e71af2fec4
Shreyash Soni <yashsoni96@gmail.com>
2024-07-30 20:07:37 +0530

    minor refactor

--- 005_fastapi/fastapi_loan_app.py
+++ 005_fastapi/fastapi_loan_app.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 
 
 app = FastAPI()
-model_name = 'RF_Loan_model.joblib'
+model_name = "RF_Loan_model.joblib"
 model = joblib.load(model_name)
 
 
@@ -24,31 +24,31 @@ class LoanPred(BaseModel):
 	TotalIncome: float
 
 
-@app.get('/')
+@app.get("/")
 def index():
-    return {'message': 'Welcome to Loan Prediction App'}
+    return {"message": "Welcome to Loan Prediction App"}
 
 
 # defining the function which will make the prediction using the data which the user inputs 
-@app.post('/predict')
+@app.post("/predict")
 def predict_loan_status(loan_details: LoanPred):
 	data = loan_details.model_dump()
-	gender = data['Gender']
-	married = data['Married']
-	dependents = data['Dependents']
-	education = data['Education']
-	self_employed = data['Self_Employed']
-	loan_amt = data['LoanAmount']
-	loan_term = data['Loan_Amount_Term']
-	credit_hist = data['Credit_History']
-	property_area = data['Property_Area']
-	income = data['TotalIncome']
+	gender = data["Gender"]
+	married = data["Married"]
+	dependents = data["Dependents"]
+	education = data["Education"]
+	self_employed = data["Self_Employed"]
+	loan_amt = data["LoanAmount"]
+	loan_term = data["Loan_Amount_Term"]
+	credit_hist = data["Credit_History"]
+	property_area = data["Property_Area"]
+	income = data["TotalIncome"]
 
 	# Making predictions 
 	prediction = model.predict([[gender, married, dependents, education, self_employed, loan_amt, loan_term, credit_hist, property_area,income]])
-	pred = 'Rejected' if prediction == 0 else 'Approved'
-	return {'Status of Loan Application': pred}
+	pred = "Rejected" if prediction == 0 else "Approved"
+	return {"Status of Loan Application": pred}
 
 
-if __name__ == '__main__':
-	uvicorn.run(app, host='127.0.0.1', port=8000)
+if __name__ == "__main__":
+	uvicorn.run(app, host="127.0.0.1", port=8000)
--- 005_fastapi/train_loan_eligibility_model.py
+++ 005_fastapi/train_loan_eligibility_model.py
@@ -11,10 +11,10 @@ RANDOM_SEED = 6
 
 # load the dataset
 dataset = pd.read_csv("train.csv")
-numerical_cols = dataset.select_dtypes(include=['int64','float64']).columns.tolist()
-categorical_cols = dataset.select_dtypes(include=['object']).columns.tolist()
-categorical_cols.remove('Loan_Status')
-categorical_cols.remove('Loan_ID')
+numerical_cols = dataset.select_dtypes(include=["int64","float64"]).columns.tolist()
+categorical_cols = dataset.select_dtypes(include=["object"]).columns.tolist()
+categorical_cols.remove("Loan_Status")
+categorical_cols.remove("Loan_ID")
 
 # Filling categorical columns with mode
 for col in categorical_cols:
@@ -28,12 +28,12 @@ for col in numerical_cols:
 dataset[numerical_cols] = dataset[numerical_cols].apply(lambda x: x.clip(*x.quantile([0.05, 0.95])))
 
 # Log Transforamtion & Domain Processing
-dataset['LoanAmount'] = np.log(dataset['LoanAmount']).copy()
-dataset['TotalIncome'] = dataset['ApplicantIncome'] + dataset['CoapplicantIncome']
-dataset['TotalIncome'] = np.log(dataset['TotalIncome']).copy()
+dataset["LoanAmount"] = np.log(dataset["LoanAmount"]).copy()
+dataset["TotalIncome"] = dataset["ApplicantIncome"] + dataset["CoapplicantIncome"]
+dataset["TotalIncome"] = np.log(dataset["TotalIncome"]).copy()
 
 # Dropping ApplicantIncome and CoapplicantIncome
-dataset = dataset.drop(columns=['ApplicantIncome','CoapplicantIncome'])
+dataset = dataset.drop(columns=["ApplicantIncome","CoapplicantIncome"])
 
 # Label encoding categorical variables
 for col in categorical_cols:
@@ -41,11 +41,11 @@ for col in categorical_cols:
     dataset[col] = le.fit_transform(dataset[col])
 
 #Encode the target columns
-dataset['Loan_Status'] = le.fit_transform(dataset['Loan_Status'])
+dataset["Loan_Status"] = le.fit_transform(dataset["Loan_Status"])
 
 # Train test split
-X = dataset.drop(columns=['Loan_Status', 'Loan_ID'])
-y = dataset.Loan_Status
+X = dataset.drop(columns=["Loan_Status", "Loan_ID"])
+y = dataset["Loan_Status"]
 
 #X_train, X_test, y_train, y_test = train_test_split(X,y, test_size =0.3, random_state = RANDOM_SEED)
 
@@ -53,10 +53,10 @@ y = dataset.Loan_Status
 rf = RandomForestClassifier(random_state=RANDOM_SEED)
 
 param_grid_forest = {
-    'n_estimators': [200,400, 700],
-    'max_depth': [10,20,30],
-    'criterion' : ["gini", "entropy"],
-    'max_leaf_nodes': [50, 100]
+    "n_estimators": [200,400, 700],
+    "max_depth": [10,20,30],
+    "criterion" : ["gini", "entropy"],
+    "max_leaf_nodes": [50, 100]
 }
 
 grid_forest = GridSearchCV(
@@ -64,15 +64,15 @@ grid_forest = GridSearchCV(
         param_grid=param_grid_forest, 
         cv=5, 
         n_jobs=-1, 
-        scoring='accuracy',
+        scoring="accuracy",
         verbose=0
     )
 
 model_forest = grid_forest.fit(X, y)
 
-joblib.dump(model_forest, 'RF_Loan_model.joblib')
+joblib.dump(model_forest, "RF_Loan_model.joblib")
 
-loaded_model = joblib.load('RF_Loan_model.joblib')
+loaded_model = joblib.load("RF_Loan_model.joblib")
 
 data = [[
     1.0,
